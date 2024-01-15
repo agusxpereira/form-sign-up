e1bae886ecdc5a5f0802f26207644458d4c88592
Agustin Pereira <aguspereira@protonmail.com>
2024-01-15 16:08:22 -0300

    funcionalidad en el input, detalles en el texto

--- main.js
+++ main.js
@@ -1,11 +1,89 @@
 let inputs = document.querySelectorAll('input'); 
+let spans = document.querySelectorAll('span');            
+
+let isOver; 
+function addClass(id){
+    spans.forEach(span =>{
+        if(isOver == true) {
+            let num = id;
+            if(num == span.id){
+                span.classList.add('target');
+            }
+        }
+    });
+
+}
+function removeClass(id){
+    spans.forEach(span =>{
+        if(isOver == false) {
+            let num = id;
+            if(num == span.id){
+                span.classList.remove('target')
+            }
+        }
+    });
+}
+
 
 inputs.forEach(input => {
-    console.log(input)
-    input.addEventListener("focus", ()=>{
-        let spans = document.querySelectorAll('span');
-        spans.forEach(span =>{
-            span.classList.toggle('target')
-        });
+    let inFocus;
+
+    input.addEventListener("mouseover", (event)=>{
+        isOver = true;
+        inFocus = false;
+        if(event.target.placeholder == ""){
+            event.target.placeholder = "you@example.com";
+        }
+        addClass(event.target.id);
     });
+
+    input.addEventListener("mouseleave", (e)=>{
+        isOver = false;
+        if(inFocus == false){
+            removeClass(e.target.id);
+            if(e.target.placeholder == "you@example.com"){
+                e.target.placeholder = "";
+            }
+        }
+        });
+
+    input.addEventListener("focus", (event)=>{
+        isOver = true;
+        inFocus = true;
+        addClass(event.target.id);
+        if(event.target.placeholder == ""){
+            event.target.placeholder = "you@example.com";
+        }
+        //addClass(event.input.id)
+        
+    }); 
+    input.addEventListener("blur",(e)=>{
+        isOver = false;
+        inFocus = false;
+        if(e.target.value == ""){
+            removeClass(e.target.id);
+            if(e.target.placeholder == "you@example.com"){
+                e.target.placeholder = "";
+            }
+        } 
+    }); 
+
+
+    input.addEventListener("click",(e)=>{
+        isOver = true;
+        addClass(e.target.id);
+
+        if(e.target.placeholder == ""){
+            e.target.placeholder = "you@example.com";
+        }
+    })
+    
 });
+
+/*
+focus/blur: 
+https://es.javascript.info/focus-blur 
+events input: 
+https://es.javascript.info/events-change-input
+
+*/
